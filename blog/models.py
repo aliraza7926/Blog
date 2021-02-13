3b52eac828887fb524e98c7bef192c4732972892
Alireza Sadeghi <aliraza7926@gmail.com>
2021-02-13 07:15:13 +0330

    rename Post field from autor to author and fix __str__ function

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=200)
-    autor=models.ForeignKey(
+    author=models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
     body=models.TextField()
 
     def __str__(self):
-        return self.titel
+        return self.title
